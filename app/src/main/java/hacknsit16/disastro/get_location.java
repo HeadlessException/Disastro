fe5c327dbe8c805964fe0c049e82485995cb95d5
JeeveshN <narang.jeevesh@gmail.com>
2016-04-09 17:52:08 +0530

    Get Location

--- app/src/main/java/hacknsit16/disastro/get_location.java
+++ app/src/main/java/hacknsit16/disastro/get_location.java
@@ -1,7 +1,52 @@
 package hacknsit16.disastro;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+
 /**
  * Created by hp on 09-Apr-16.
  */
-public class get_location {
+public class get_location implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    GoogleApiClient mGoogleApiClient;
+    Location mLastLocation;
+    double lat,longi;
+
+    public void make(Context context) {
+        mGoogleApiClient = new GoogleApiClient.Builder(context)
+                .addConnectionCallbacks((GoogleApiClient.ConnectionCallbacks) context)
+                .addOnConnectionFailedListener((GoogleApiClient.OnConnectionFailedListener) context)
+                .addApi(LocationServices.API)
+                .build();
+    }
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        try {
+            mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        }
+        catch (SecurityException e) {
+        }
+        if (mLastLocation != null) {
+            lat=mLastLocation.getLatitude();
+            longi=mLastLocation.getLongitude();
+        }
+    }
+
 }
